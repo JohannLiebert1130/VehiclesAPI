43393376635e3a0abd0bdce8830e9e4597195978
JohannLiebert1130 <672016243@qq.com>
2020-06-24 19:03:57 +0800

    init a eureka server

---
+++ eureka-server/src/test/java/com/johann/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johann.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
